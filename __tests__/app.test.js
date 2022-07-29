f2b1e51cd0a2c4c6a34a6550cbc9f532041ba9c7
jenna-graham <jclayto2@gmail.com>
2022-07-28 17:30:22 -0700

    testing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,12 +3,27 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+describe('crystal routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('GET /crystals should return a list of crystals', async () => {
+    const resp = await request(app).get('/crystals');
+    expect(resp.body.length).toEqual(8);
+    expect(resp.body[0]).toEqual({
+      id: expect.any(String),
+      name: expect.any(String),
+    });
+  });
+  it('GET /crystals/:id should return the crystal detail', async () => {
+    const resp = await request(app).get('/crystals/2');
+    expect(resp.body).toEqual({
+      id: '2',
+      name: 'Amethyst',
+      color: 'purple',
+      zodiac: 'Capricorn',
+      properties: 'Spirituality and meditation',
+    });
   });
   afterAll(() => {
     pool.end();
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,21 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP table if exists crystals;
+
+CREATE table crystals (
+    id BIGINT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR NOT NULL,
+    color VARCHAR NOT NULL,
+    zodiac VARCHAR NOT NULL,
+    properties VARCHAR NOT NULL
+);
+
+INSERT INTO crystals (name, color, zodiac, properties) VALUES
+('Snowflake Obsidian', 'black/white', 'Virgo', 'purity and balance'),
+('Amethyst', 'purple', 'Capricorn', 'Spirituality and meditation'),
+('Carnelean', 'red', 'Taurus', 'Stabilizing energies'),
+('Malachite', 'green', 'Scorpio', 'Transformations'),
+('Rose Quartz', 'pink', 'Taurus', 'Emotional healing'),
+('Moonstone', 'beige', 'Libra', 'New beginnings and confidence'),
+('Citrine', 'yellow', 'Aries', 'Attracts and maintains wealth'),
+('Tigers eye', 'brown', 'Capricorn', 'Clarity and optimism');
